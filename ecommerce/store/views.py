575d999d016cd6beed1a18fa885625cb045a1ca0
Julian Wong <julian.df.wong@gmail.com>
2023-04-27 14:58:19 +0800

    created store page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Product
+# .models because we have the Product models in the same store app
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter(is_available=True)
+    product_count =  products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    # context will make this available for the html page
+
+    return render(request, 'store/store.html', context)
